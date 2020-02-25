39243611ff6c09391ecad9ddb1d2f9653e7d6c8a
yuya.mohri <shion0430@gmail.com>
2020-02-25 02:11:11 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :following, :followers]
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
@@ -26,6 +26,18 @@ class UsersController < ApplicationController
       render :new
     end  
   end
+  
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end  
   
   private
   
